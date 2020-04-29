a4c06aa12d9c2eb0e79277cfad41e98034ef0f28
BingTian <chittytian93@gmail.com>
2020-04-28 21:41:56 -0700

    optimize the algorithm solution

--- Encrypt.java
+++ Encrypt.java
@@ -1,76 +1,60 @@
-import java.util.ArrayList;
-import java.util.*;
-
 public class Encrypt {
     public static void main(String args[]){
-        String s = "if ";
-        System.out.println(encrypt(s));
+        String s = "if man was meant to stay on the ground god would have given us roots";
+        Encrypt encrypt = new Encrypt();
+        System.out.println(encrypt.encrypt(s));
 
     }
-    public static String encrypt(String s){
-        // The basic idea is extract the words in s, then calculate the sqrt of the length (truncate the decimal
-        // part only remain the integer part) to decide the dimension to store the string. Put the string into such
-        // a dimension area, then read by columns and out put the final encrypt result.
+    public String encrypt(String s){
+        // The basic idea is to count the letters in s, then calculate the sqrt of the length (truncate the decimal
+        // part only remain the integer part) to decide how many columns to store the string. Each column is a bucket
+        // to store the corresponding column letters. Finally we just join the string in each bucket together to get the final string.
         //Time complexity is O(n), n represents the length of string s. Space complexity is O(n).
 
-        //Remove the space in s and get the length of the string
-        StringBuilder sb = new StringBuilder();
-        String[] seg = s.split("[ ]+");
-        for (String str : seg){
-            if (str != ""){
-                sb.append(str);
+        // Count total letters and decide the number of buckets
+        int totalLetters = countCharacters(s);
+        int root = (int) Math.sqrt(totalLetters);
+        int bucket = root + 1;
+        if (root*root == totalLetters) {
+            bucket = root;
+        }
+        StringBuilder[] cols = new StringBuilder[bucket];
+        //Use a count to mark the index of each letter, then calculate idx as a marker to decide which bucket should the letter go.
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!Character.isAlphabetic(c)) {
+                continue;
             }
+            int idx = count%bucket;
+            if (cols[idx] == null) {
+                cols[idx] = new StringBuilder();
+            }
+            cols[idx].append(c);
+            count++;
         }
-        int len = sb.length();
-        //Do the sqrt operation and decide the dimension of the matrix to store the string
-        int low = sqrt(len);
-        int up = low+1;
-        if (low*low == len){
-            up = low;
-        }
-        if (low*up < len){
-            low = up;
-        }
-        //First encrypt the string to list res according to the dimension mentioned above
-        List<String> res = new ArrayList<>();
-        int lastLine = 0;
-        for (int i = 0; i<len; i+=up){
-            if (i+up > len-1){
-                res.add(sb.substring(i));
-                lastLine = sb.substring(i).length();//Record the length of the last line
+        //Sum each bucket up to get the final string
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i< cols.length; i++) {
+            if (i == cols.length-1){
+                res.append(cols[i].toString());
             }else{
-                res.add(sb.substring(i, i+up));
+                res.append(cols[i].toString());
+                res.append(" ");
             }
         }
-        //Next read the final result by columns
-        StringBuilder result = new StringBuilder();
-        for (int col = 0; col<up; col++){
-            StringBuilder vertical = new StringBuilder();
-            for (int row = 0; row<low; row++){
-                if (col>lastLine-1 && row == low-1){//If the col is beyond the length of last line
-                    continue;                       //&& at the last line, we do nothing but continue
-                }else{
-                    vertical.append(res.get(row).charAt(col));
-                }
-            }
-            vertical.append(" ");
-            result.append(vertical);
-        }
-        return result.toString();
+        return res.toString();
     }
-    public static int sqrt(int x){
-        if (x == 0) return 0;
-        if (x == 1) return 1;
-        long l = 1, r = x;
-        while (l<r) {
-            long mid = l+(r-l+1)/2;
-            if (mid*mid <= x){
-                l = mid;
-            }else{
-                r = mid-1;
+
+    private int countCharacters(final String s) {
+        if (s == null) return 0;
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isAlphabetic(s.charAt(i))) {
+                total++;
             }
         }
-        return (int) l;
+        return total;
     }
 
 }
